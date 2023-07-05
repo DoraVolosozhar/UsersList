8c09d95689e58c764c92efaf662370521076d599
Volososhar <daryatestakk.2021@gmail.com>
2023-07-05 15:56:11 +0300

    First commit

---
+++ lib/ui/users_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+class UsersList extends StatelessWidget {
+
+  const UsersList({super.key});
+
+  static Widget render() {
+    return ChangeNotifierProvider(create: (BuildContext context) {  },
+
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+  
+}
